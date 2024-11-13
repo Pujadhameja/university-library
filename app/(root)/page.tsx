ba98930639613420323946cca7682be02ac39915
sujatagunale <sujata@jsmastery.pro>
2024-11-13 19:22:50 +0530

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <p className="text-5xl font-semibold">Dan Origin</p>;
+};
+
+export default Home;
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "ibm-plex-sans": ["IBM Plex Sans", "sans-serif"],
+      },
+      colors: {},
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
