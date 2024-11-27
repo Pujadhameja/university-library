107b59d51741bb0eb7ea54e57b5d36265412b526
Cienias <cieja0@gmail.com>
2024-11-27 15:52:10 +0100

    fix: dashboard's sidebar scaling on small screens

--- components/dashboard/nav/Sidebar.tsx
+++ components/dashboard/nav/Sidebar.tsx
@@ -11,14 +11,14 @@ const Sidebar = () => {
   return (
     <div className="sticky left-0 top-0 flex h-dvh min-w-fit flex-col justify-between border bg-white px-4">
       <div className="divide-y-2 divide-dashed">
-        <div className="flex flex-row items-center gap-2 py-6">
+        <div className="flex flex-row items-center gap-2 py-6 max-md:justify-center">
           <Image
             src="/assets/icons/dashboard/logo.svg"
             height={37}
             width={37}
             alt="site-logo"
           />
-          <h1 className="text-3xl font-semibold text-dashboard-primary">
+          <h1 className="text-3xl font-semibold text-dashboard-primary max-md:hidden">
             BookWise
           </h1>
         </div>
@@ -33,7 +33,7 @@ const Sidebar = () => {
             return (
               <Link key={link.route} href={link.route} className="">
                 <div
-                  className={`flex flex-row items-center ${isSelected && "bg-dashboard-primary shadow-sm"} w-full gap-2 rounded-md px-3 py-2`}
+                  className={`flex flex-row items-center ${isSelected && "bg-dashboard-primary shadow-sm"} w-full gap-2 rounded-md px-3 py-2 max-md:justify-center`}
                 >
                   <Image
                     src={isSelected ? link.selectedImg : link.img}
@@ -42,7 +42,7 @@ const Sidebar = () => {
                     alt="icon"
                   />
                   <p
-                    className={`text-xl font-semibold text-dark-200 ${isSelected && "text-white"}`}
+                    className={`text-xl font-semibold text-dark-200 ${isSelected && "text-white"} max-md:hidden`}
                   >
                     {link.text}
                   </p>
@@ -52,7 +52,7 @@ const Sidebar = () => {
           })}
         </div>
       </div>
-      <div className="my-8 flex w-full flex-row gap-2 rounded-full border border-light-600 px-6 py-2 shadow-sm">
+      <div className="my-8 flex w-full flex-row gap-2 rounded-full border border-light-600 px-6 py-2 shadow-sm max-md:px-2">
         <Image
           src="/assets/images/dashboard/admin.png"
           alt="auth-user"
@@ -60,7 +60,7 @@ const Sidebar = () => {
           height={48}
           className="size-[48px] "
         />
-        <div className="flex flex-col">
+        <div className="flex flex-col max-md:hidden">
           <p className="text-lg font-semibold text-dark-200">Adrian Hajdin</p>
           <p className="text-light-200">adrian@jsmastery.pro</p>
         </div>
