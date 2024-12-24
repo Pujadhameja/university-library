3c1d6dbd46b920b4dd4e03eb1c3ad776d853699b
sujatagunale <sujata@jsmastery.pro>
2024-12-24 23:55:23 +0530

    add admin check

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,14 +1,36 @@
-import Sidebar from "@/components/admin/Sidebar";
+import { eq } from "drizzle-orm";
 import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-const AdminLayout = async ({ children }: { children: ReactNode }) => (
-  <main className="flex min-h-screen w-full flex-row">
-    <Sidebar />
+import { auth } from "@/auth";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import Sidebar from "@/components/admin/Sidebar";
 
-    <div className="flex w-[calc(100%-264px)] flex-1 flex-col bg-light-400 p-5 xs:p-10">
-      {children}
-    </div>
-  </main>
-);
+const AdminLayout = async ({ children }: { children: ReactNode }) => {
+  const session = await auth();
+  if (!session?.user?.id) redirect("/sign-in");
+
+  const isAdmin = await db
+    .select({
+      isAdmin: users.role,
+    })
+    .from(users)
+    .where(eq(users.id, session.user?.id))
+    .limit(1)
+    .then((res) => res[0].isAdmin === "ADMIN");
+
+  if (!isAdmin) redirect("/");
+
+  return (
+    <main className="flex min-h-screen w-full flex-row">
+      <Sidebar />
+
+      <div className="flex w-[calc(100%-264px)] flex-1 flex-col bg-light-400 p-5 xs:p-10">
+        {children}
+      </div>
+    </main>
+  );
+};
 
 export default AdminLayout;
--- database/schema.ts
+++ database/schema.ts
@@ -9,6 +9,7 @@ import {
   uuid,
 } from "drizzle-orm/pg-core";
 
+export const ROLE_ENUM = pgEnum("role", ["USER", "ADMIN"]);
 const STATUS_ENUM = pgEnum("status", ["PENDING", "APPROVED", "REJECTED"]);
 
 export const users = pgTable("users", {
@@ -19,6 +20,7 @@ export const users = pgTable("users", {
   password: text("password").notNull(),
   universityCard: text("university_card").notNull(),
   status: STATUS_ENUM("status").default("PENDING"),
+  role: ROLE_ENUM("role").default("USER"),
   lastActivityDate: date("last_activity_date").defaultNow(),
   createdAt: timestamp("created_at", {
     withTimezone: true,
